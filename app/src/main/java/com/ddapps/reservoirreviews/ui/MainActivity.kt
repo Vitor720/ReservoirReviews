07b211562a672c45400fb76397db067eb27036b3
Vitor Vieira <nun720@gmail.com>
2020-09-13 13:54:58 -0300

    Adicionado menu lateral

--- app/src/main/java/com/ddapps/reservoirreviews/ui/MainActivity.kt
+++ app/src/main/java/com/ddapps/reservoirreviews/ui/MainActivity.kt
@@ -2,12 +2,32 @@ package com.ddapps.reservoirreviews.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.ddapps.reservoirreviews.R
+import com.ddapps.reservoirreviews.databinding.ActivityMainBinding
 import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val navController = this.findNavController(R.id.produtividade_nav)
+        drawerLayout = binding.drawerLayout
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        NavigationUI.setupWithNavController(binding.navView, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.produtividade_nav)
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
