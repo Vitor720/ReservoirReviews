d243580afca031a3fae9623367accab8f74fd2a5
Vitor Vieira <nun720@gmail.com>
2020-09-14 04:01:52 -0300

    Refatorada função de remoção de favoritos

--- app/src/main/java/com/ddapps/reservoirreviews/ui/viewmodel/FavoriteViewModel.kt
+++ app/src/main/java/com/ddapps/reservoirreviews/ui/viewmodel/FavoriteViewModel.kt
@@ -2,12 +2,14 @@ package com.ddapps.reservoirreviews.ui.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.ddapps.reservoirreviews.domain.common.model.MovieDisplay
 import com.ddapps.reservoirreviews.domain.common.networking.Resource
 import com.ddapps.reservoirreviews.domain.usecase.FavoritesReviewsUseCase
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import timber.log.Timber
 
 class FavoriteViewModel(private val useCase: FavoritesReviewsUseCase): ViewModel() {
 
@@ -20,6 +22,12 @@ class FavoriteViewModel(private val useCase: FavoritesReviewsUseCase): ViewModel
             favoritesList.postValue(Resource.loading(null))
             val response = useCase.getAllFavoriteReviews()
             favoritesList.postValue(response)
+        }
+    }
+
+    fun removeTitleFromFavorites(movieTitle: String) {
+        viewModelScope.launch {
+            useCase.removeFavoriteMovie(movieTitle)
         }
     }
 
